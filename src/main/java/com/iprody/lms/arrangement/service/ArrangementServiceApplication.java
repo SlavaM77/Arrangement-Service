3342f9fcb8b6d530456046e2646e169df9ced74b
Malyshev Yaroslav <yaroslav.malyshev@axamit.com>
2024-09-10 11:24:30 +0300

    init commit

---
+++ src/main/java/com/iprody/lms/arrangement/service/ArrangementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iprody.lms.arrangement.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrangementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrangementServiceApplication.class, args);
+	}
+
+}
